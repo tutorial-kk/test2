bb85323f1619365e3a51024bde7e52bc54aed4f0
kk(kibera) <kk(kibera)@kkkaberi-PC>
2017-09-22 03:57:33 +0900

    first commit

---
+++ hello/src/hello/Code01.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class Code01 {
+
+	static int num;
+	
+	public static void main(String[] args) {
+		int num2=5;
+		num=2;
+		
+		System.out.println(num + " " + num2);
+		System.out.println(num );
+		System.out.println(num2);
+		
+
+	}
+
+}
---
+++ hello/src/hello/Hello2.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world !!!!!!");
+		System.out.println("00-000000000");
+		System.out.println("00-000000000");
+
+	}
+
+}
